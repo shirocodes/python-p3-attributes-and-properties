1ea76fe75151454ddb7c346d9e4ec536050211b1
shirocodes <ishirapt@gmail.com>
2025-05-12 17:47:27 +0300

    Implemented property() function.

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = 'Bosco', breed = 'Pug'):
+        self._name = None  # Ensure attribute exists
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print('Name must be string between 1 and 25 characters.')
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+fido = Dog('Fido', 'Pug')
+print(fido.name)
+print(fido.breed)
+
+fido.name = ''
+fido.breed = 'Bosco'
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'Shiro', job = 'Software engineer'):
+        self._name = None  # Placeholder before setter
+        self._job = None   # Placeholder before setter
+        self.name = name   # Will trigger setter validation
+        self.job = job     # Will trigger setter validation
+
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1<= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+user = Person('User', 'doctor')
+print(user.name)
+print(user.job)
+
+        
